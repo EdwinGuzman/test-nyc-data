8dc5845b30a9dceecb348e1046b4741e411e69e6
Edwin Guzman <edwin.gzmn@gmail.com>
2022-10-06 17:11:58 -0400

    Getting nyc leading death data and displaying it

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+
 import './App.css';
 
 function App() {
+  const leadingCauseOfDeathData = "https://data.cityofnewyork.us/resource/jb7j-dtam.json";
+  const [deathData, setDeathData] = useState([]);
+
+  useEffect(() => {
+    fetch(leadingCauseOfDeathData)
+      .then((response) => {
+        if (response.status === 404) {
+          return "something happened and we couldn't get the file";
+        }
+        return response.json()
+      })
+      .then((data) => {
+        // console.log(data)
+        setDeathData(data)
+      })
+      .catch((error) => {
+        console.log(error)
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {
+          deathData.map((deathObject, index) => {
+            // age_adjusted_death_rate
+            // death_rate
+            // deaths
+            // leading_cause
+            // race_ethnicity
+            // sex
+            // year
+            return (
+              <li key={index}>
+                <p>Leading Cause: {deathObject.leading_cause}</p>
+                <p>Year: {deathObject.year}</p>
+                <p>Race Ethnicity: {deathObject.race_ethnicity}</p>
+              </li>
+            )
+          })
+        }
+      </ul>
     </div>
   );
 }
